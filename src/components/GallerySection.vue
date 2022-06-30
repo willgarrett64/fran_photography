28fb8e9b3237207648df90e91c6743f548dd2d73
Will <willgarrett64@yahoo.com>
2022-06-30 22:28:25 +0100

    feat: add placeholder gallery category images

--- src/components/GallerySection.vue
+++ src/components/GallerySection.vue
@@ -3,26 +3,26 @@
     <div class="h-full w-full flex items-center justify-between space-x-5">
       <div>
         <h4>Pregnancy</h4>
-        <div>
-          <img src="" alt="">
+        <div class="image__card">
+          <img src="../assets/placeholders/portrait-3.jpg" alt="">
         </div>
       </div>
       <div>
         <h4>Newborn</h4>
-        <div>
-          <img src="" alt="">
+        <div class="image__card">
+          <img src="../assets/placeholders/newborn-p-1.jpg" alt="">
         </div>
       </div>
       <div>
         <h4>Family</h4>
-        <div>
-          <img src="" alt="">
+        <div class="image__card">
+          <img src="../assets/placeholders/family-p-1.jpg" alt="">
         </div>
       </div>
       <div>
         <h4>Portraits</h4>
-        <div>
-          <img src="" alt="">
+        <div class="image__card">
+          <img src="../assets/placeholders/portrait-p-1.jpg" alt="">
         </div>
       </div>
     </div>
@@ -41,6 +41,15 @@ export default {
 <style scoped>
 .cover {
   @apply w-screen h-screen;
+  object-fit: cover;
+}
+
+.image__card {
+  @apply w-44 h-52 drop-shadow;
+}
+
+.image__card img {
+  @apply w-full h-full rounded;
   object-fit: cover;
 }
 </style>
