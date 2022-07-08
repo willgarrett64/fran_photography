8063523b61a0301691487801bb51b9220c6e9d63
Will <willgarrett64@yahoo.com>
2022-07-08 22:57:07 +0100

    feat: add padding to gallery cards

--- src/components/GallerySection.vue
+++ src/components/GallerySection.vue
@@ -1,32 +1,31 @@
 <template>
-  <section class="cover relative sm:pt-20 parallax gallery flex flex-col justify-center" id="gallery">
-    <div class="overlay" />
-    <h2 class="text-2xl mb-10">Gallery</h2>
-    <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between sm:space-x-5 sm:px-40 w-full h-full sm:h-auto">
+  <section class="cover relative sm:pt-20 parallax gallery h-screen" id="gallery">
+    <h2 class="text-2xl py-4 arima">Gallery</h2>
+    <div class="gallery__list">
       <div class="image__card__holder">
         <div class="overlay sm:hidden"/>
-        <div class="card__title"><h4>Pregnancy</h4></div>
+        <div class="card__title arima"><h4>Pregnancy</h4></div>
         <div class="image__card">
           <img src="../assets/placeholders/portrait-3.jpg" alt="">
         </div>
       </div>
       <div class="image__card__holder">
         <div class="overlay sm:hidden"/>
-        <div class="card__title"><h4>Newborn</h4></div>
+        <div class="card__title arima"><h4>Newborn</h4></div>
         <div class="image__card">
           <img src="../assets/placeholders/newborn-p-1.jpg" alt="">
         </div>
       </div>
       <div class="image__card__holder">
         <div class="overlay sm:hidden"/>
-        <div class="card__title"><h4>Family</h4></div>
+        <div class="card__title arima"><h4>Family</h4></div>
         <div class="image__card">
           <img src="../assets/placeholders/family-p-1.jpg" alt="">
         </div>
       </div>
       <div class="image__card__holder">
         <div class="overlay sm:hidden"/>
-        <div class="card__title"><h4>Portraits</h4></div>
+        <div class="card__title arima"><h4>Portraits</h4></div>
         <div class="image__card">
           <img src="../assets/placeholders/portrait-p-1.jpg" alt="">
         </div>
@@ -51,13 +50,13 @@ export default {
 }
 
 .image__card__holder {
-  @apply relative h-1/4 sm:h-auto overflow-hidden;
+  @apply relative h-1/4 rounded sm:h-auto overflow-hidden;
 }
 .image__card__holder .card__title {
   @apply absolute text-white text-2xl h-full w-full flex items-center justify-center sm:relative sm:text-gray-500;
 }
 .image__card {
-  @apply sm:w-64 sm:h-96 sm:rounded-xl overflow-hidden;
+  @apply sm:w-64 sm:h-96 rounded sm:rounded-xl overflow-hidden;
   box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
 }
 
@@ -67,6 +66,11 @@ export default {
 }
 
 .gallery {
-  background-image: url("../assets/placeholders/brickWall-l-1.jpg");
+  /* background-image: url("../assets/placeholders/brickWall-l-1.jpg"); */
+}
+
+.gallery__list {
+  @apply flex flex-col sm:flex-row sm:items-center sm:justify-between sm:space-x-5 px-4 pb-4 space-y-2 sm:px-40 w-full sm:h-auto;
+  height: calc(100vh - 64px);
 }
 </style>
