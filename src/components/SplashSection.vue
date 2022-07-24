0c23dda21caaa7039926e01c9b52f9229d15c68f
Will Garrett <willgarrett64@yahoo.com>
2022-07-24 21:41:51 +0100

    feat: show logo on desktop

--- src/components/SplashSection.vue
+++ src/components/SplashSection.vue
@@ -2,7 +2,7 @@
   <section class="cover relative">
     <div class="parallax splash"></div>
     <div class="title">
-      <img src="../assets/FranPhotographyLogo.jpeg" alt="logo" srcset="" class="hidden">
+      <img src="../assets/FranPhotographyLogo.jpeg" alt="logo" srcset="" class="hidden md:block h-64">
       <h1 class="header dancing-script">Capturando o que é invisível aos olhos.</h1>
       <h2 class="hidden sub-header">Give me money and I will take your photo.</h2>
     </div>
@@ -27,20 +27,28 @@ export default {
 
 .title {
   @apply absolute text-3xl w-full;
-  top: 25vh;
+  top: 22vh;
 }
 
 .header {
-  @apply text-2xl px-1;
+  @apply text-4xl px-1 px-2;
 }
 
 .sub-header {
   @apply text-xl;
 }
 
-@screen sm {
+@screen md {
   .splash {
     background-image: url("../assets/placeholders/splash-l-1.jpg")
+  }
+
+  .title {
+    @apply w-1/2 flex flex-col items-center space-y-6 absolute left-1/2 text-white font-bold pt-10 pr-4;
+  }
+
+  .header {
+    @apply text-4xl;
   }
 }
 </style>
