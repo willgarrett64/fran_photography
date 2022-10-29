8664f0cc7faee726b010e1164866b073ce172b4e
Will Garrett <willgarrett64@yahoo.com>
2022-10-29 09:35:50 +0100

    feat: minor changes to slidehow wip

--- src/components/GallerySlideshow.vue
+++ src/components/GallerySlideshow.vue
@@ -5,7 +5,7 @@
       <XIcon class="w-6"/>
     </button>
     <div class="flex">
-      <button @click="prevImage"><ChevronLeftIcon class="w-10 mr-4"/></button>
+      <button @click="prevImage"><ChevronLeftIcon class="w-5 sm:w-8 md:w-10 sm:mr-4"/></button>
       <div class="panel">
         <img
           :src="getSrc(images[viewingIndex])"
@@ -13,7 +13,7 @@
         />
       </div>
         </div>
-        <button @click="nextImage"><ChevronRightIcon class="w-10 ml-4"/></button>
+        <button @click="nextImage"><ChevronRightIcon class="w-5 sm:w-8 md:w-10 sm:ml-4"/></button>
     </div>
 </template>
 
@@ -61,19 +61,23 @@ export default {
 
 <style scoped>
 .modal-overlay {
-  @apply fixed top-0 h-screen w-screen bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 py-10 px-40;
+  @apply fixed top-0 left-0 h-screen w-screen bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 sm:py-10 sm:px-20 md:py-10 md:px-40;
 }
 .panel {
   @apply inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-3xl sm:w-full;
+  max-height: 85vh;
 }
 
 button {
   @apply text-white
 }
 button.close {
-  @apply absolute top-10 right-10 text-white text-lg flex items-end space-x-2;
+  @apply absolute top-5 right-5 sm:top-10 sm:right-10 text-white text-lg flex items-end space-x-2;
 }
-button:hover {
-  color: var(--fran-blue);
+
+@media (hover: hover) {
+  button:hover {
+    color: var(--fran-blue);
+  }
 }
 </style>
